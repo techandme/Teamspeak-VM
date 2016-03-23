8d4c22c34435d3cd5a8b15a9aadc4012076f235d
Ezra <ezraholm50@gmail.com>
2016-03-23 11:16:46 +0100

    Update change-teamspeak-profile.sh

--- change-teamspeak-profile.sh
+++ change-teamspeak-profile.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+#
+## Tech and Me ## - ©2016, https://www.techandme.se/
+#
+ROOT_PROFILE="/root/.bash_profile"
 
+rm /root/.profile
+
+cat <<-ROOT-PROFILE > "$ROOT_PROFILE"
+# ~/.profile: executed by Bourne-compatible login shells.
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
+fi
+if [ -x /var/scripts/setup.sh ]; then
+        /var/scripts/setup.sh
+fi
+if [ -x /var/scripts/history.sh ]; then
+        /var/scripts/history.sh
+fi
+mesg n
+ROOT-PROFILE
