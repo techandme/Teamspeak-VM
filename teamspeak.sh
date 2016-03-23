45091f7ce101f20244d10aac0e6b20bb0b610e4e
Ezra <ezraholm50@gmail.com>
2016-03-23 09:58:55 +0100

    Update teamspeak.sh

--- teamspeak.sh
+++ teamspeak.sh
@@ -1,11 +1,41 @@
+
+#PID_TS=(ps -ef | grep "teamspeak3" | awk '{print $2}')
+
+# Add user
 adduser --disabled-login teamspeak3
+
+# Get Teamspeak
 wget http://ftp.4players.de/pub/hosted/ts3/releases/3.0.10.3/teamspeak3-server_linux-amd64-3.0.10.3.tar.gz -p /tmp
+
+# Unpack Teamspeak
 tar xzf /tmp/teamspeak3-server_linux-amd64-3.0.10.3.tar.gz
-sudo mv /tmp/teamspeak3-server_linux-amd64 /usr/local/teamspeak3
-sudo chown -R teamspeak3 /usr/local/teamspeak3
-sudo ln -s /usr/local/teamspeak3/ts3server_startscript.sh /etc/init.d/teamspeak3
-sudo update-rc.d teamspeak3 defaults
 
+# Move to right directory
+mv /tmp/teamspeak3-server_linux-amd64 /usr/local/teamspeak3
 
-enter if your paying attention and are able to copy paste this info
-sudo service teamspeak3 start
+# Set ownership
+chown -R teamspeak3 /usr/local/teamspeak3
+
+# Add to upstart
+ln -s /usr/local/teamspeak3/ts3server_startscript.sh /etc/init.d/teamspeak3
+update-rc.d teamspeak3 defaults
+
+# Warning
+echo -e "\e[32m"
+echo    "+--------------------------------------------------------------------+"
+echo    "| Next you will need to copy/paste 3 things to a safe location       |"
+echo    "|                                                                    |"
+echo -e "|         \e[0mLogin, password, security token\e[32m                 |"
+echo    "|                                                                    |"
+echo -e "|         \e[0mIF YOU FAIL TO DO SO, YOU HAVE TO REINSTALL YOUR SYSTEM\e[32m          |"
+echo    "|                                                                    |"
+echo -e "|    \e[91mPress Ctrl + C when your done to finish te script\e[32m    |"
+echo    "+--------------------------------------------------------------------+"
+echo
+read -p "Press any key to start copying the important stuff to a safe location..." -n1 -s
+echo -e "\e[0m"
+echo
+
+# Start service
+service teamspeak3 start
+#sleep 60 && kill -INT $PID_TS | service teamspeak3 start
