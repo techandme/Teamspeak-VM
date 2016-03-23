5784135cf83526239fc07775382a9543321e920a
Ezra <ezraholm50@gmail.com>
2016-03-23 11:26:55 +0100

    Update change-root-profile.sh

--- change-root-profile.sh
+++ change-root-profile.sh
@@ -2,33 +2,22 @@
 #
 ## Tech and Me ## - ©2016, https://www.techandme.se/
 #
-TEAMSPEAK_PROFILE="/home/teamspeak/.bash_profile"
+ROOT_PROFILE="/root/.bash_profile"
 
-rm /home/teamspeak/.profile
+rm /root/.profile
 
-cat <<-TEAMSPEAK-PROFILE > "$TEAMSPEAK_PROFILE"
-# ~/.profile: executed by the command interpreter for login shells.
-# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
-# exists.
-# see /usr/share/doc/bash/examples/startup-files for examples.
-# the files are located in the bash-doc package.
-# the default umask is set in /etc/profile; for setting the umask
-# for ssh logins, install and configure the libpam-umask package.
-#umask 022
-# if running bash
-if [ -n "$BASH_VERSION" ]; then
-    # include .bashrc if it exists
-    if [ -f "$HOME/.bashrc" ]; then
-        . "$HOME/.bashrc"
-    fi
+cat <<-ROOT-PROFILE > "$ROOT_PROFILE"
+# ~/.profile: executed by Bourne-compatible login shells.
+if [ "$BASH" ]; then
+  if [ -f ~/.bashrc ]; then
+    . ~/.bashrc
+  fi
 fi
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/bin" ] ; then
-    PATH="$HOME/bin:$PATH"
+if [ -x /var/scripts/setup.sh ]; then
+        /var/scripts/setup.sh
 fi
-bash /var/scripts/instruction.sh
-bash /var/scripts/history.sh
-sudo -i
-TEAMSPEAK-PROFILE
-
-exit 0
+if [ -x /var/scripts/history.sh ]; then
+        /var/scripts/history.sh
+fi
+mesg n
+ROOT-PROFILE
