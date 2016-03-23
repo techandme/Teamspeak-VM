ae082d90ddd3c55f5cd8364605c70859d855a9b6
Ezra <ezraholm50@gmail.com>
2016-03-23 10:51:45 +0100

    Update welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -1,1 +1,16 @@
-
+#!/bin/bash
+#
+## Tech and Me ## - ©2016, https://www.techandme.se/
+#
+WANIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+ADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+clear
+figlet -f small Tech and Me
+echo "           https://www.techandme.se"
+echo 
+echo
+echo
+echo "WAN IP: $WANIP"
+echo "LAN IP: $ADDRESS"
+echo
+exit 0
