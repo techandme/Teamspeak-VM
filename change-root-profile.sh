e488523873d88f10224e09ada34ef831531436cf
Ezra <ezraholm50@gmail.com>
2016-03-23 11:15:52 +0100

    Update change-root-profile.sh

--- change-root-profile.sh
+++ change-root-profile.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
+#
+## Tech and Me ## - ©2016, https://www.techandme.se/
+#
+TEAMSPEAK_PROFILE="/home/teamspeak/.bash_profile"
 
+rm /home/teamspeak/.profile
+
+cat <<-TEAMSPEAK-PROFILE > "$TEAMSPEAK_PROFILE"
+# ~/.profile: executed by the command interpreter for login shells.
+# This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login
+# exists.
+# see /usr/share/doc/bash/examples/startup-files for examples.
+# the files are located in the bash-doc package.
+# the default umask is set in /etc/profile; for setting the umask
+# for ssh logins, install and configure the libpam-umask package.
+#umask 022
+# if running bash
+if [ -n "$BASH_VERSION" ]; then
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+        . "$HOME/.bashrc"
+    fi
+fi
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+bash /var/scripts/instruction.sh
+bash /var/scripts/history.sh
+sudo -i
+TEAMSPEAK-PROFILE
+
+exit 0
