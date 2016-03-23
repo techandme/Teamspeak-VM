4a8a9afd58f61c18cb57bd1220bca6d8dc30882a
Ezra <ezraholm50@gmail.com>
2016-03-23 09:56:54 +0100

    Update test_connection.sh

--- test_connection.sh
+++ test_connection.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+#
+## Tech and Me ## - ©2016, https://www.techandme.se/
+#
 WGET="/usr/bin/wget"
 
 $WGET -q --tries=20 --timeout=10 http://www.google.com -O /tmp/google.idx &> /dev/null
