962b5f3f4d6d3f4198859b33c4e70339ed9d1e6c
Ezra <ezraholm50@gmail.com>
2016-03-23 10:36:09 +0100

    Delete set_nameserver.sh

--- set_nameserver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-## Tech and Me ## - ©2016, https://www.techandme.se/
-#
-# Change DNS
-echo "nameserver 8.26.56.26" > /etc/resolvconf/resolv.conf.d/base
-echo "nameserver 8.20.247.20" >> /etc/resolvconf/resolv.conf.d/base
