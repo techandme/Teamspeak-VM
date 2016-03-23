4abffc0d5fbd6506e1cde1393c2acae8ce0b1f73
Ezra <ezraholm50@gmail.com>
2016-03-23 10:49:42 +0100

    Update instruction.sh

--- instruction.sh
+++ instruction.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+#
+## Tech and Me ## - ©2016, https://www.techandme.se/
+#
+clear
+cat << INST1
++-----------------------------------------------------------------------+
+| Thank you for downloading this Teamspeak VM made by Tech and Me!       |
+|                                                                       |
+INST1
+echo -e "|"  "\e[32mTo run the startup script, just type: the sudo password\e[0m               |"
+echo -e "|"  "\e[32mThe sudo password is: teamspeak, or the PW you chose during install\e[0m    |"
+cat << INST2
+|                                                                       |
+|  ####################### Tech and Me - 2016 ########################  |
++-----------------------------------------------------------------------+
+INST2
 
+exit 0
